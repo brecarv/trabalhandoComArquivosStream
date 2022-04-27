b9169b601590cec7e8cb743ae489755c9d036ce1
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-27 16:58:48 -0300

    directoryInfo

---
+++ ReadDirectories/Program.cs
@@ -0,0 +1,27 @@
+﻿var path = @"D:\dev-workspace\DIO-Impulso-Fullstack\trabalhandoComArquivosStream\DirectoryAndDirectoryInfo\globo";
+
+ReadDirectories(path);
+Console.WriteLine("Digite enter para finalizar.");
+Console.ReadLine();
+static void ReadDirectories(string path)
+{
+
+  if (Directory.Exists(path))
+  {
+    var directories = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+    foreach (var dir in directories)
+    {
+      var dirInfo = new DirectoryInfo(dir);
+      Console.WriteLine($"[Nome]:{dirInfo.Name}");
+      Console.WriteLine($"[Raiz]:{dirInfo.Root}");
+      if (dirInfo.Parent != null)
+        Console.WriteLine($"[Pai]:{dirInfo.Parent.Name}");
+
+      Console.WriteLine("-----------------------------");
+    }
+  }
+  else
+  {
+    Console.WriteLine($"{path} não existe!");
+  }
+}
