7d25d7522cf02b468afc2d583475fb8625765e69
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-28 16:05:31 -0300

    csvStreamReader

---
+++ csvStream/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+var path = Path.Combine(
+  Environment.CurrentDirectory,
+  "Entrada",
+  "usuarios-exportacao.csv");
+if (File.Exists(path))
+{
+  using var sr = new StreamReader(path);
+  var cabecalho = sr.ReadLine()?.Split(',');
+
+  while (true)
+  {
+    var registro = sr.ReadLine()?.Split(',');
+    if (registro == null) break;
+    if (cabecalho?.Length != registro.Length)
+    {
+      WriteLine("Arquivo fora do padrão.");
+      break;
+    }
+    for (int i = 0; i < registro.Length; i++)
+    {
+      WriteLine($"{cabecalho?[i]}:{registro[i]}");
+    }
+    WriteLine("---------------------------");
+  }
+}
+else
+{
+  WriteLine($"O arquivo {path} não existe.");
+}
+WriteLine("\n\n Pressione [enter] para sair...");
+ReadLine();
