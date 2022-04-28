d6e89b6ba5bd13d151f4fa9b0cdeefb95c762170
Emerson Carvalho <8569519+brecarv@users.noreply.github.com>
2022-04-28 16:18:10 -0300

    csvStreamWriter, criando arquivos

--- csvStream/Program.cs
+++ csvStream/Program.cs
@@ -1,33 +1,86 @@
 ﻿using static System.Console;
 
-var path = Path.Combine(
+CriarCsv();
+
+WriteLine("\n\n Pressione [enter] para sair...");
+ReadLine();
+
+static void CriarCsv()
+{
+  var path = Path.Combine(
+  Environment.CurrentDirectory,
+  "Saida");
+
+  var pessoas = new List<Pessoa>() {
+    new Pessoa(){
+      Nome = "Emerson Carvalho",
+      Email = "ec@ec.com",
+      Telefone = 123456
+    },
+    new Pessoa(){
+      Nome = "Emanuel Carvalho",
+      Email = "emanuelc@ec.com",
+      Telefone = 1123123
+    },
+    new Pessoa(){
+      Nome = "Vagna Carvalho",
+      Email = "vc@ec.com",
+      Telefone = 1125231
+    },
+  };
+
+  var di = new DirectoryInfo(path);
+  if (!di.Exists)
+  {
+    di.Create();
+    path = Path.Combine(path, "usuarios.csv");
+  }
+  using var sw = new StreamWriter(path);
+  sw.WriteLine("nome,email,telefone");
+  foreach (var pessoa in pessoas)
+  {
+    var linha = $"{pessoa.Nome},{pessoa.Email},{pessoa.Telefone}";
+    sw.WriteLine(linha);
+  }
+}
+
+static void LerCsv()
+{
+  var path = Path.Combine(
   Environment.CurrentDirectory,
   "Entrada",
   "usuarios-exportacao.csv");
-if (File.Exists(path))
-{
-  using var sr = new StreamReader(path);
-  var cabecalho = sr.ReadLine()?.Split(',');
-
-  while (true)
+  if (File.Exists(path))
   {
-    var registro = sr.ReadLine()?.Split(',');
-    if (registro == null) break;
-    if (cabecalho?.Length != registro.Length)
+    using var sr = new StreamReader(path);
+    var cabecalho = sr.ReadLine()?.Split(',');
+
+    while (true)
     {
-      WriteLine("Arquivo fora do padrão.");
-      break;
+      var registro = sr.ReadLine()?.Split(',');
+      if (registro == null) break;
+      if (cabecalho?.Length != registro.Length)
+      {
+        WriteLine("Arquivo fora do padrão.");
+        break;
+      }
+      for (int i = 0; i < registro.Length; i++)
+      {
+        WriteLine($"{cabecalho?[i]}:{registro[i]}");
+      }
+      WriteLine("---------------------------");
     }
-    for (int i = 0; i < registro.Length; i++)
-    {
-      WriteLine($"{cabecalho?[i]}:{registro[i]}");
-    }
-    WriteLine("---------------------------");
+  }
+  else
+  {
+    WriteLine($"O arquivo {path} não existe.");
   }
 }
-else
+
+class Pessoa
 {
-  WriteLine($"O arquivo {path} não existe.");
+  public string Nome { get; set; }
+  public string Email { get; set; }
+  public int Telefone { get; set; }
+
 }
-WriteLine("\n\n Pressione [enter] para sair...");
-ReadLine();
